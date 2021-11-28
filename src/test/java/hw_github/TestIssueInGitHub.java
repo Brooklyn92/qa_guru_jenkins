93f5b82e89555e46d572f3f88e77d487e4feed42
evgeniashennikova <evgeniashenikova@gmail.com>
2021-11-28 20:09:00 +0300

    Убрала лишний Configuration

--- src/test/java/hw_github/TestIssueInGitHub.java
+++ src/test/java/hw_github/TestIssueInGitHub.java
@@ -1,9 +1,11 @@
 package hw_github;
+
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,34 +16,33 @@ public class TestIssueInGitHub {
     private static final Integer ISSUE_NUMBER_SEARCH = 68;
 
     @BeforeAll
-    public static void set(){
-        Configuration.startMaximized=true;
-        Configuration.holdBrowserOpen=true;
+    public static void set() {
+        Configuration.startMaximized = true;
     }
 
     @Test
-    public void testGit(){
+    public void testGit() {
         step("Открываем главную страницу GitHub", () -> {
             open("https://github.com/");
         });
 
-        step("Ищем репозиторий", () ->{
+        step("Ищем репозиторий", () -> {
             $(".header-search-input").click();
             $(".header-search-input").setValue(REPOSITORYNAME);
             $(".header-search-input").submit();
         });
 
-        step("Переходим в репозиторий", ()-> {
+        step("Переходим в репозиторий", () -> {
             $(By.linkText(REPOSITORYNAME)).click();
         });
 
 
-        step("Переходим во вкладку Issue", ()-> {
+        step("Переходим во вкладку Issue", () -> {
             $("#issues-tab").click();
         });
 
 
-        step("Убеждаемся, что номер issue отображается", ()-> {
+        step("Убеждаемся, что номер issue отображается", () -> {
             $(withText("#" + ISSUE_NUMBER_SEARCH)).should(Condition.visible);
         });
     }
