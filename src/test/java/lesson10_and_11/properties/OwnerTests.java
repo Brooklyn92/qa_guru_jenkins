1c7b8e1dfd0eed4cedefcd04ff8e4905841ee7e1
Brooklyn92 <Tubanar173russia>
2021-12-07 21:51:48 +0400

    add ownerTests

---
+++ src/test/java/lesson10_and_11/properties/OwnerTests.java
@@ -0,0 +1,21 @@
+package lesson10_and_11.properties;
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+import static java.lang.String.format;
+
+public class OwnerTests {
+    public CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    @Test
+    void readCredentialsTest(){
+    String login = credentials.login();
+    String password = credentials.password();
+        System.out.println(login);
+        System.out.println(password);
+
+        String message = format("i'm login as %s with password", login, password);
+        System.out.println(message);
+    }
+}
