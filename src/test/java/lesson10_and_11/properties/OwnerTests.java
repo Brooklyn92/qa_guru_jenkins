1effb0853408d9df05fb68bdb8ced75362b07024
Brooklyn92 <Tubanar173russia>
2021-12-07 21:54:46 +0400

    add ownerTests1

--- src/test/java/lesson10_and_11/properties/OwnerTests.java
+++ src/test/java/lesson10_and_11/properties/OwnerTests.java
@@ -1,9 +1,11 @@
 package lesson10_and_11.properties;
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static java.lang.String.format;
 
+@Tag("properties")
 public class OwnerTests {
     public CredentialsConfig credentials =
             ConfigFactory.create(CredentialsConfig.class);
